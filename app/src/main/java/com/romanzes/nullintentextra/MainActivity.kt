8f3042341326c71b7a4064fa259ddfb0dae44071
Roman Petrenko <roman@canva.com>
2018-07-01 16:26:53 +1000

    initial commit

---
+++ app/src/main/java/com/romanzes/nullintentextra/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.romanzes.nullintentextra
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            startActivity(
+                    Intent(this, SecondActivity::class.java)
+                            .also { it.putExtra(EXTRA_TEXT, editText.text.toString()) }
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/romanzes/nullintentextra/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.romanzes.nullintentextra
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
+
+const val EXTRA_TEXT = "text"
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val text = intent.getStringExtra(EXTRA_TEXT)
+        textResult.text = getString(R.string.text_result, text)
+    }
+}
